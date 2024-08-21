65f178ba4026fdc0a8d099fcdb06b7e326f58dcd
Carvalanga <stratilatov.ea@phystech.edu>
2024-08-21 15:21:18 +0300

    Сделал работу со структурой

--- square_solver.cpp
+++ square_solver.cpp
@@ -5,18 +5,19 @@
 #include "equation_type.h"
 #include "input_output.h"
 #include "solver.h"
+#include "square_equation_struct.h"
 
 int main(){
     printf("#Программа решения квадратных уравнений\n#Введите через пробел коээфициенты квадратного уравнения:\n");
 
-    double first_coef = 0, second_coef = 0, third_coef = 0;
+    sqr_eq eq = {};
 
-    data_input(&first_coef, &second_coef, &third_coef);
+    data_input(&eq.cfs.first, &eq.cfs.second, &eq.cfs.third);
 
-    double root1 = 0, root2 = 0;
+    //double root1 = 0, root2 = 0;
 
-    equation_type root_count = square_solver(first_coef, second_coef, third_coef, &root1, &root2);
-    int output = show_result(root_count, root1, root2);
+    equation_type root_count = square_solver(eq.cfs.first, eq.cfs.second, eq.cfs.third, &eq.rts.x1, &eq.rts.x2);
+    int output = show_result(root_count, eq.rts.x1, eq.rts.x2);
 
     return output;
 }
