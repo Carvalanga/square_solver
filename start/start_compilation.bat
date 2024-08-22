197920a5e61a9c696e32e3c56ca8be430846d683
Carvalanga <stratilatov.ea@phystech.edu>
2024-08-22 10:22:26 +0300

    Перенес bat-файлы в отдельную папку

--- start/start_compilation.bat
+++ start/start_compilation.bat
@@ -1,2 +1,2 @@
+cd..
 g++ square_solver.cpp input_output.cpp unit_test.cpp solver.cpp -o square.exe -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wno-missing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -Werror=vla -D_DEBUG -D_EJUDGE_CLIENT_SIDE
-square.exe
